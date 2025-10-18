8b590e759a2f9e224e457b2471155c2500e0fe95
Your Name <khobragadebarkha26@gmail.com>
2025-10-19 01:23:48 +0530

    Register models in admin.py

--- alumni/admin.py
+++ alumni/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Alumni, Event, Announcement
+
+@admin.register(Alumni)
+class AlumniAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'graduation_year', 'profession', 'contact_number')
+    search_fields = ('name', 'email', 'graduation_year', 'profession')
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'date', 'created_at')
+    search_fields = ('title',)
+
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'posted_at')
+    search_fields = ('title',)
