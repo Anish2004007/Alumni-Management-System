9f57dd52941ee351ca55e72c00549d6db3873b65
Aayushi shinde <aayushishinde2004@gmail.com>
2025-10-25 14:06:19 +0530

    added forms.py with forms for alumni,events models

---
+++ alumni/forms.py
@@ -0,0 +1,18 @@
+alumni/forms.py
+from django import forms
+from .models import Alumni, Event, Announcement
+class AlumniForm(forms.ModelForm):
+     class Meta:
+            model = Alumni
+            fields = ['name', 'email', 'graduation_year', 'profession', 'work_history', 'contact_number']
+            widgets = {
+                    'work_history': forms.Textarea(attrs={'rows': 3}),
+            }
+class EventForm(forms.ModelForm):
+        class Meta:
+            model = Event
+            fields = ['title', 'description', 'date']
+            widgets = {
+                    'description': forms.Textarea(attrs={'rows': 3}),
+                    'date': forms.DateInput(attrs={'type': 'date'}),
+        }
